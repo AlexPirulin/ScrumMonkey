68f3b02e1e3200254d7ccc68e6619334f03d4f04
a385520 <a385520@uach.mx>
2026-04-20 18:10:21 -0600

    CRUD

--- js/app.js
+++ js/app.js
@@ -26,5 +26,54 @@ function addProject() {
   console.log("Proyecto agregado:", newProject);
 }
 
+let proyectos = [];
+let editando = -1;
+
+function guardarProyecto() {
+    let input = document.getElementById("nombreProyecto");
+    let nombre = input.value.trim();
+
+    if(nombre === ""){
+        alert("Escribe un proyecto");
+        return;
+    }
+
+    if(editando === -1){
+        proyectos.push(nombre);
+    }else{
+        proyectos[editando] = nombre;
+        editando = -1;
+    }
+
+    input.value = "";
+    mostrarProyectos();
+}
+
+function mostrarProyectos(){
+    let lista = document.getElementById("listaProyectos");
+    lista.innerHTML = "";
+
+    proyectos.forEach((proyecto,index)=>{
+        lista.innerHTML += `
+            <li>
+                ${proyecto}
+                <div class="acciones">
+                    <button onclick="editarProyecto(${index})">Editar</button>
+                    <button onclick="eliminarProyecto(${index})">Eliminar</button>
+                </div>
+            </li>
+        `;
+    });
+}
+
+function editarProyecto(index){
+    document.getElementById("nombreProyecto").value = proyectos[index];
+    editando = index;
+}
+
+function eliminarProyecto(index){
+    proyectos.splice(index,1);
+    mostrarProyectos();
+}
 // Evento del botón
 document.getElementById("btnAdd").addEventListener("click", addProject);
